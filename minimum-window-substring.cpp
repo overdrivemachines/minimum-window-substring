738acd3b769d1404d1121f1bab7354543f7c8079
get.dipen@gmail.com <get.dipen@gmail.com>
2023-02-11 10:18:07 -0800

    found valid window

--- minimum-window-substring.cpp
+++ minimum-window-substring.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include <unordered_set>
 
 using namespace std;
@@ -6,6 +7,92 @@ using namespace std;
 // return a substring of s such that all chars of t are in it
 // the substring should be of the smallest length possible
 string minWindow(string s, string t) {
+  map<char, int> t_map;
+  map<char, int> window_map;
+  int left_index = 0, right_index;
+  char left_char, right_char;  // characters at s[left_index] and s[right_index]
+  int s_length = s.length(), t_length = t.length();
+  int have_chars = 0, need_chars;
+  int min_window_length = s_length;
+  int min_window_left_index, min_window_right_index;
+
+  cout << "s = " << s << " t = " << t << "\n";
+
+  if (t_length > s_length)
+    return "";
+
+  // t_map contains the count of each character in t
+  for (char c : t)
+    t_map[c]++;
+  // t_map[c] = (t_map.count(c)) ? (t_map[c] + 1) : 1;
+  need_chars = t_map.size();
+
+  cout << "Need Chars: " << need_chars << "\n";
+
+  left_char = s[0];
+  for (right_index = 0; right_index < s_length - 1; right_index++) {
+    // window string is s[left_index..right_index]
+    // window string = s.substr(left_index, (right_index - left_index))
+
+    // s = "AAZZZAAABBBCCCCC", t = "BBBCCCA"
+
+    right_char = s[right_index];
+    // if t_map['A'] > 0, add 'A' to window_map
+    if (t_map.count(right_char)) {
+      window_map[right_char]++;
+      if (window_map[right_char] == t_map[right_char]) {
+        // if window_map['A'] == t_map['A'] increament have_chars
+        have_chars++;
+
+        // Check if window is valid
+        if (have_chars == need_chars) {
+          // Valid window
+          cout << "Valid Window: " << left_index << "-" << right_index << ": " << s.substr(left_index, right_index - left_index + 1) << "\n";
+
+          // check if current window is smaller in length than previously found valid window
+          if ((right_index - left_index) < min_window_length) {
+            // current window is the shortest valid window seen so far
+            min_window_length = right_index - left_index + 1;
+            min_window_left_index = left_index;
+            min_window_right_index = right_index;
+
+            // move the left index until have_chars is less than need_chars
+            while ((left_index <= right_index) && (have_chars == need_chars)) {
+              left_char = s[left_index];
+              if (window_map[left_char] > t_map[left_char]) {
+                left_index++;
+                window_map[left_char]--;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // check if window is valid.
+    // for a window to be valid it must contain all the characters in t
+  }
+
+  return "";
+}
+
+int main(int argc, char const *argv[]) {
+  string s;
+  string t;
+  // cout << minWindow("hello", "hello") << "\n";
+  // cout << minWindow("hello", "hellwewo") << "\n";
+  // cout << minWindow("worldhelloworld", "hello") << "\n";
+  // cout << minWindow("helloworld", "jack") << "\n";
+  // cout << minWindow("ADOBECODEBANC", "ABC") << "\n";
+  // cout << minWindow("AAAAF", "FGH") << "\n";
+  // cout << minWindow("bba", "ab") << "\n";
+  // cout << minWindow("babb", "baba") << "\n";
+  cout << minWindow("AAZZZAAABBBCCCCC", "BBBCCCA") << "\n";
+
+  return 0;
+}
+
+string minWindowSlow(string s, string t) {
   string result;
   string tOriginal = t;
   // cout << "s = " << s << endl;
@@ -116,20 +203,4 @@ string minWindow(string s, string t) {
   // cout << "minWindowStartPos: " << minWindowStartPos << "\n";
   // cout << "minWindowLength: " << minWindowLength << "\n";
   return s.substr(minWindowStartPos, minWindowLength);
-}
-
-int main(int argc, char const *argv[]) {
-  string s;
-  string t;
-  // cout << minWindow("hello", "hello") << "\n";
-  // cout << minWindow("hello", "hellwewo") << "\n";
-  // cout << minWindow("worldhelloworld", "hello") << "\n";
-  // cout << minWindow("helloworld", "jack") << "\n";
-  // cout << minWindow("ADOBECODEBANC", "ABC") << "\n";
-  // cout << minWindow("AAAAF", "FGH") << "\n";
-  // cout << minWindow("bba", "ab") << "\n";
-  // cout << minWindow("babb", "baba") << "\n";
-  cout << minWindow("AAZZZAAABBBCCCCC", "BBBCCCA") << "\n";
-
-  return 0;
 }
